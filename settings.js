619fc231eb4b7b8d528cc84c31b26033a3d7e493
Dasyn133 <ponnamaleesha818@gmail.com>
2026-05-22 16:34:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3f63d6bfedf22e39' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3f63d6bfef22e39' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
+d
